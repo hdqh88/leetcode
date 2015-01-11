f3e4efbe317bae77221b1798ca3528e6b5c52abd
Bo Liu <emmaliu.2010@gmail.com>
2015-01-11 18:06:55 -0500

    Update TwoSumII-Inputarrayissorted.java

--- TwoSumII-Inputarrayissorted/TwoSumII-Inputarrayissorted.java
+++ TwoSumII-Inputarrayissorted/TwoSumII-Inputarrayissorted.java
@@ -11,44 +11,44 @@
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-// Assume input is already sorted.
-for (int i = 0; i < numbers.length; i++) {
-int j = bsearch(numbers, target - numbers[i], i + 1);
-if (j != -1) {
-return new int[] { i + 1, j + 1 };
-}
-}
-throw new IllegalArgumentException("No two sum solution");
-}
-private int bsearch(int[] A, int key, int start) {
-int L = start, R = A.length - 1;
-while (L < R) {
-int M = (L + R) / 2;
-if (A[M] < key) {
-L = M + 1;
-} else {
-R = M;
-}
-}
-return (L == R && A[L] == key) ? L : -1;
-}
+        // Assume input is already sorted.
+        for (int i = 0; i < numbers.length; i++) {
+            int j = bsearch(numbers, target - numbers[i], i + 1);
+            if (j != -1) {
+                return new int[] { i + 1, j + 1 };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    private int bsearch(int[] A, int key, int start) {
+        int L = start, R = A.length - 1;
+        while (L < R) {
+            int M = (L + R) / 2;
+            if (A[M] < key) {
+                L = M + 1;
+            } else {
+                R = M;
+            }
+        }
+        return (L == R && A[L] == key) ? L : -1;
+    }
 }
 
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-// Assume input is already sorted.
-int i = 0, j = numbers.length - 1;
-while (i < j) {
-int sum = numbers[i] + numbers[j];
-if (sum < target) {
-i++;
-} else if (sum > target) {
-j--;
-} else {
-return new int[] { i + 1, j + 1 };
-}
-}
-throw new IllegalArgumentException("No two sum solution");
-}
+        // Assume input is already sorted.
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                return new int[] { i + 1, j + 1 };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 }
