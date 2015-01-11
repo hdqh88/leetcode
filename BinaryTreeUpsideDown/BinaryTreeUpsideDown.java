0ba149a3943a2fc13a6c18db3e34aaf685486688
Bo Liu <emmaliu.2010@gmail.com>
2015-01-11 17:46:18 -0500

    Update BinaryTreeUpsideDown.java

--- BinaryTreeUpsideDown/BinaryTreeUpsideDown.java
+++ BinaryTreeUpsideDown/BinaryTreeUpsideDown.java
@@ -20,13 +20,13 @@
 
 public class Solution {
     public TreeNode UpsideDownBinaryTree(TreeNode root) {
-return dfsBottomUp(root, null);
-}
-private TreeNode dfsBottomUp(TreeNode p, TreeNode parent) {
-if (p == null) return parent;
-TreeNode root = dfsBottomUp(p.left, p);
-p.left = (parent == null) ? parent : parent.right;
-p.right = parent;
-return root;
-}
+        return dfsBottomUp(root, null);
+    }
+    private TreeNode dfsBottomUp(TreeNode p, TreeNode parent) {
+        if (p == null) return parent;
+        TreeNode root = dfsBottomUp(p.left, p);
+        p.left = (parent == null) ? parent : parent.right;
+        p.right = parent;
+        return root;
+    }
 }
