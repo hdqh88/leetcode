7322195948c57aeb6024a57e4d207fa7ad69d8cb
Bo Liu <emmaliu.2010@gmail.com>
2015-01-11 17:45:32 -0500

    Create TwoSumIII-Datastructuredesign.java

---
+++ TwoSumIII-Datastructuredesign/TwoSumIII-Datastructuredesign.java
@@ -0,0 +1,32 @@
+//========================================================================================================================
+//Design and implement a TwoSum class. It should support the following operations: add and find.
+//
+//add - Add the number to an internal data structure.
+//find - Find if there exists any pair of numbers which sum is equal to the value.
+//
+//For example,
+//add(1); add(3); add(5);
+//find(4) -> true
+//find(7) -> false
+//========================================================================================================================
+
+public class TwoSum {
+private Map<Integer, Integer> table = new HashMap<>();
+public void add(int input) {
+int count = table.containsKey(input) ? table.get(input) : 0;
+table.put(input, count + 1);
+}
+public boolean find(int val) {
+for (Map.Entry<Integer, Integer> entry : table.entrySet()) {
+int num = entry.getKey();
+int y = val - num;
+if (y == num) {
+// For duplicates, ensure there are at least two individual numbers.
+if (entry.getValue() >= 2) return true;
+} else if (table.containsKey(y)) {
+return true;
+}
+}
+return false;
+}
+}
