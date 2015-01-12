45249c37e28e493538ad2a122b5f50d6408c243e
Bo Liu <emmaliu.2010@gmail.com>
2015-01-11 19:38:56 -0500

    Update BinaryTreeUpsideDown.java

--- BinaryTreeUpsideDown/BinaryTreeUpsideDown.java
+++ BinaryTreeUpsideDown/BinaryTreeUpsideDown.java
@@ -30,3 +30,15 @@ public class Solution {
         return root;
     }
 }
+public TreeNode UpsideDownBinaryTree(TreeNode root) {
+    TreeNode p = root, parent = null, parentRight = null;
+    while (p != null) {
+        TreeNode left = p.left;
+        p.left = parentRight;
+        parentRight = p.right;
+        p.right = parent;
+        parent = p;
+        p = left;
+    }
+    return parent;
+}
