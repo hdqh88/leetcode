d3693309545f153f417151fb54fefa8234ad2e51
Howard <hdqh88@gmail.com>
2015-01-30 11:56:50 -0500

    Update RemoveDuplicatesfromSortedArrayII.cpp

--- RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.cpp
+++ RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.cpp
@@ -29,7 +29,7 @@ public:
     
     int removeDuplicates2(int A[], int n){
         int N = 2;
-        if(n < N)
+        if(n <= N)
             return n;
         int index = 0;
         int dup = 1;
