82252bb7b8e025b83a668f8c30f1c76d90c270b3
Howard <hdqh88@gmail.com>
2015-02-02 11:33:40 -0500

    Update AddTwoNumbers.cpp

--- AddTwoNumbers/AddTwoNumbers.cpp
+++ AddTwoNumbers/AddTwoNumbers.cpp
@@ -51,6 +51,28 @@ public:
         if (sum > 0) cur->next = new ListNode(sum);
         return popDummy(l3);
     }
+    
+    
+    
+    // Hao
+    ListNode *addTwoNumbers2(ListNode *l1, ListNode *l2) {
+         ListNode *head = new ListNode(0);
+        ListNode *p = head;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL){
+            int v1 = l1 == NULL ? 0 : l1->val;
+            int v2 = l2 == NULL ? 0 : l2->val;
+            int value = (v1 + v2 + carry) % 10;
+            carry = (v1 + v2 + carry) / 10;
+            p -> next = new ListNode(value);
+            p = p->next;
+            l1 = l1 == NULL ? NULL : l1->next;
+            l2 = l2 == NULL ? NULL : l2->next;
+        }
+        if(carry)
+            p -> next = new ListNode(carry);
+        return head -> next;
+    }
 };
 
 
