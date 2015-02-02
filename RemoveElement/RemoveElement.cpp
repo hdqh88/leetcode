f97e3160b70ba977677802fe567a0ce19d8350fe
Howard <hdqh88@gmail.com>
2015-02-01 21:20:25 -0500

    Update RemoveElement.cpp

--- RemoveElement/RemoveElement.cpp
+++ RemoveElement/RemoveElement.cpp
@@ -20,6 +20,17 @@ public:
         }
         return j;
     }
+    
+    
+    // Hao
+    int removeElement2(int A[], int n, int elem) {
+        int index = 0;
+        for(int i = 0; i < n; i++){
+            if(A[i] != elem)
+                A[index++] = A[i];
+        }
+        return index;
+    }
 };
 
 int main() {
