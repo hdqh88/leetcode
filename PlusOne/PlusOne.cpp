0ce000f2dfe8566c155c438db4245fae12879570
Howard <hdqh88@gmail.com>
2015-02-01 23:07:36 -0500

    Update PlusOne.cpp

--- PlusOne/PlusOne.cpp
+++ PlusOne/PlusOne.cpp
@@ -25,6 +25,28 @@ public:
         if (i == -1) res.insert(res.begin(), 1);
         return res;
     }
+    
+    
+    
+    // Hao
+    vector<int> plusOne2(vector<int> &digits) {
+        int carry = 0;
+        vector<int>::iterator iter = digits.end() - 1;
+        int value = (*iter + carry + 1) % 10;
+        carry = (*iter + carry + 1) / 10;
+        *iter-- = value;
+        while(iter >= digits.begin()){
+            if(carry == 0)
+                break;
+            value = (*iter + carry) % 10;
+            carry = (*iter + carry) / 10;
+            *iter-- = value;
+        }
+        if(carry)
+            digits.insert(digits.begin(), 1);
+        return digits;
+    }
+    
 };
 
 int main() {
